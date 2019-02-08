5dc5e316278a439b2a79c7762b870bdedaae17c9
priyank96 <priyankbhandia@hotmail.com>
2019-02-08 21:10:54 +0530

    bug fixes, added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Deep-Portfolio',
+    version='0.0',
+    packages=['autoEncoder', 'makeDataset.live', 'makeDataset.pandafy', 'makeDataset.querySide',
+              'makeDataset.similarity', 'makeDataset.companyWise'],
+    url='',
+    license='',
+    author='216_230_725',
+    author_email='',
+    description='Deep-Portfolio'
+)

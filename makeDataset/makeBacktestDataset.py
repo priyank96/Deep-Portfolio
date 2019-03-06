a4fc18d83b6f4d87a08ef51806ee996ec44ab8c7
priyank96 <priyankbhandia@hotmail.com>
2019-03-06 22:46:57 +0530

    finding common time frame for dataset

--- makeDataset/makeBacktestDataset.py
+++ makeDataset/makeBacktestDataset.py
@@ -1,11 +1,19 @@
-import pickle 
-with open('./processedData/CompanyWiseDict.pkl','rb') as fil:
+import pickle
+
+with open('./processedData/CompanyWiseDict.pkl', 'rb') as fil:
     company_wise_dict = pickle.load(fil)
 
-# find the earliest start date available in all datasets
-# find the latest end date available in all datasets
+# find the list of timestamps in all files
+keys = company_wise_dict.keys()
+values_set = None
+for key in keys:
+    if len(company_wise_dict[key]) > 0:
+        timestamps = set(list([x[0] for x in company_wise_dict[key]]))
+        if values_set is None:
+            values_set = timestamps
+        else:
+            values_set = values_set.intersection(timestamps)
 
+print(len(values_set))
 
-
-# trim the lists of these dicts so they start and end at the same time
-
+# now make a dataframe with

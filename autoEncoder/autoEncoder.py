56dc00dd91427541468d88338d8fed3f8c9b778e
priyank96 <priyankbhandia@hotmail.com>
2019-03-10 17:31:15 +0530

    bug fixes in autoEncoder

--- autoEncoder/autoEncoder.py
+++ autoEncoder/autoEncoder.py
@@ -1,11 +1,11 @@
 import pickle
-import keras
-from keras.layers import Dense, Input
+
 from keras.layers import CuDNNLSTM
+from keras.layers import Dense
 from keras.layers import TimeDistributed
-from keras.models import Sequential
+from keras.models import Sequential, load_model, Model
 from numpy import array
-import os
+
 
 class AutoEncoder:
     def train(dataset):
@@ -13,34 +13,34 @@ class AutoEncoder:
             sequence = pickle.load(f)
         sequence = array(sequence)
         sequence = sequence.reshape((len(sequence), 20, 4))
-        #print(sequence.shape)
+        # print(sequence.shape)
 
         model = Sequential()
-        model.add(CuDNNLSTM(128, input_shape=(20, 4), return_sequences=True)) # encode 1
-        model.add(CuDNNLSTM(256,  return_sequences=True)) # encode 2
-        model.add(CuDNNLSTM(512,  return_sequences=True)) # encode 3 -- our final vector
-        model.add(CuDNNLSTM(256,  return_sequences=True)) # decode 1
-        model.add(CuDNNLSTM(128,  return_sequences=True)) # decode 2
+        model.add(CuDNNLSTM(128, input_shape=(20, 4), return_sequences=True))  # encode 1
+        model.add(CuDNNLSTM(256, return_sequences=True))  # encode 2
+        model.add(CuDNNLSTM(512, return_sequences=True))  # encode 3 -- our final vector
+        model.add(CuDNNLSTM(256, return_sequences=True))  # decode 1
+        model.add(CuDNNLSTM(128, return_sequences=True))  # decode 2
         model.add(TimeDistributed(Dense(4)))
         model.compile(optimizer='adam', loss='mse')
- 
-        #print(model.summary())
+
+        # print(model.summary())
         model.fit(sequence, sequence, epochs=1, batch_size=64, validation_split=0.2)
         model.save('model-latest.h5')
-       # model.save('old-models/model-'+str(dataset))
+
+    # model.save('old-models/model-'+str(dataset))
 
     def get_vectors(data):
-        #Load the latest model
+        # Load the latest model
         model = load_model('model-latest.h5')
-        
-        #Remove the decoder layer and set the model's output to the Encoder's output
+
+        # Remove the decoder layer and set the model's output to the Encoder's output
         model = Model(inputs=model.inputs, outputs=model.layers[3].output)
 
-        #Reshape the data
+        # Reshape the data
         sequence = array(data)
         sequence = sequence.reshape((len(sequence), 20, 4))
 
-        #Run the model on the input timeseries and get vector representation
-        vectors = model.predict(data)
+        # Run the model on the input timeseries and get vector representation
+        vectors = model.predict(sequence)
         return vectors
-        

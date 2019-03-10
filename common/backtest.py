dabc8f6be29b17fd220d003e7629621fbf33f4d3
priyank96 <priyankbhandia@hotmail.com>
2019-03-10 17:55:38 +0530

    backtest in progress

--- common/backtest.py
+++ common/backtest.py
@@ -1,11 +1,26 @@
 import pickle
+from autoEncoder.autoEncoder import AutoEncoder
 # load the testing dataset
 with open('../makeDataset/processedData/BackTestDict.pkl') as f:
     data_set = pickle.load(f)
 
+def get_full_historical_data():
+    pass
+
+def get_twenty_days_data():
+    pass
+
+def calculate_returns():
+    pass
+
+def update_performance():
+    pass
+
 # load the encoder class
+encoder = AutoEncoder()
 
 # start from 21st day in the dataset
+
 
 # vectorize last 20 days data
 # cluster the vectors
